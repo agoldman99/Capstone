c60eb6906607c56ebe03b2475cf82be4443c8dbe
agoldman99 <54633075+agoldman99@users.noreply.github.com>
2021-04-16 14:28:59 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -128,22 +128,3 @@ function mouseUp() {document.getElementById("square-grid-item1").style.backgroun
 
 
 
-
-
-
-
-/* ABOUT PLAY NAVIGATION BAR MICROINTERACTION*/
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("material-section-nav-id").style.padding = "30px 10px";
-    document.getElementById("grid-aboutplayindex-id").style.height = "180px";
-  } else {
-    document.getElementById("material-section-nav-id").style.padding = "80px 10px";
-    document.getElementById("grid-aboutplayindex-id").style.height = "275px";
-  }
-}
-
-

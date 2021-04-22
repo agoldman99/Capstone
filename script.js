8e37587958505c8bd1697d8fe0cf70bce741c054
agoldman99 <54633075+agoldman99@users.noreply.github.com>
2021-04-22 15:12:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,140 +1,141 @@
-const canvas = document.getElementById("canvas1");
+const canvas1 = document.getElementById('canvas1');
 
 /* clear canvas button */
-const clearEl = document.getElementById("clear1");
+const clearEl1 = document.getElementById('clear1');
 
-const ctx = canvas.getContext('2d');
+const ctx1 = canvas1.getContext('2d');
+
 
 /* initialize global variables */
-let radius = 3;           /* this is the circle radius and the line width */
-let isPressed = true;
-let color = '#0d2d53';
-let currPosX, currPosY; /* position of the mouse; they start out undefined */
+let radius1 = 3;           /* this is the circle radius and the line width */
+let isPressed1 = true;
+let color1 = '#0d2d53';
+let currPosX1, currPosY1; /* position of the mouse; they start out undefined */
 
 /* set up the event listeners */
-canvas.addEventListener('mousedown', (e) => {
-   isPressed = true;
+canvas1.addEventListener('mousedown', (e) => {
+   isPressed1 = true;
 
-   currPosX = e.offsetX;
-   currPosY = e.offsetY;
+   currPosX1 = e.offsetX;
+   currPosY1 = e.offsetY;
 
    /* I think we want to draw a circle when the mouse is just clicked */
    /* not just when the mouse is moved */
    /* I changed this from the original program */
-   drawCircle(currPosX, currPosY);
+   drawCircle1(currPosX1, currPosY1);
 
    // console.log(isPressed, currPosX, currPosY);
 });
 
-canvas.addEventListener('mouseup', (e) => {
-   isPressed = false;
+canvas1.addEventListener('mouseup', (e) => {
+   isPressed1 = false;
 
-   currPosX = undefined;
-   currPosY = undefined;
+   currPosX1 = undefined;
+   currPosY1 = undefined;
 
    // console.log(isPressed, currPosX, currPosY);
 }); /* end mouseup eventListener */
 
-canvas.addEventListener('mousemove', (e) => {
-   if( isPressed) {  /* we only want to do this when the mouse is held down */
-      const x2 = e.offsetX;
-      const y2 = e.offsetY;
+canvas1.addEventListener('mousemove', (e) => {
+   if( isPressed1) {  /* we only want to do this when the mouse is held down */
+      const x21 = e.offsetX;
+      const y21 = e.offsetY;
 
-      drawCircle(x2, y2); /* but this can't keep up with fast movement, so draw line also */
-      drawLine(currPosX, currPosY, x2, y2);
+      drawCircle1(x21, y21); /* but this can't keep up with fast movement, so draw line also */
+      drawLine1(currPosX1, currPosY1, x21, y21); /*if take this line away, it will draw dots*/
 
       /* we need to update the current position */
-      currPosX = x2;
-      currPosY = y2;
+      currPosX1 = x21;
+      currPosY1 = y21;
+
       // console.log(offsetX, offsetY);
    } /* end isPressed */
 
    // console.log(isPressed, currPosX, currPosY);
 }); /* end mousemove eventListener */
 
+
 /* x, y define center of circle */
 /* radius is the radius */
 /* the next two are start angle and end angle in RADIANS */
 /* 2*PI is 360 degrees */
-function drawCircle( x, y ) {
-   ctx.beginPath();
-   ctx.arc(x, y, radius, 0, Math.PI * 2);
-   ctx.fillStyle = color;
-   ctx.fill();
+function drawCircle1(x1, y1) {
+   ctx1.beginPath();
+   ctx1.arc(x1, y1, radius1, 0, Math.PI * 2);
+   ctx1.fillStyle = color1;
+   ctx1.fill();
 } /* end drawCircle() */
 
 /* draw lines in between the circles to fill in the drawing */
 /* move to the first coordinate pair, draw to the second */
-function drawLine(moveToX, moveToY, lineToX, lineToY) {
-   ctx.beginPath();
-   ctx.moveTo(moveToX, moveToY);
-   ctx.lineTo(lineToX, lineToY);
-   ctx.strokeStyle = color;
+function drawLine1(moveToX1, moveToY1, lineToX1, lineToY1) {
+   ctx1.beginPath();
+   ctx1.moveTo(moveToX1, moveToY1);
+   ctx1.lineTo(lineToX1, lineToY1);
+   ctx1.strokeStyle = color1;
 
    /* we want diameter so line width matches circle */
-   ctx.lineWidth = radius * 2;
-   ctx.stroke();                /* actually draw the line */
+   ctx1.lineWidth = radius1 * 2;
+   ctx1.stroke();                /* actually draw the line */
 }  /* end drawLine() */
 
 /* the clear button */
-clearEl.addEventListener('click', () => 
-            ctx.clearRect(0, 0, canvas.width, canvas.height));
+clearEl1.addEventListener('click', () => 
+            ctx1.clearRect(0, 0, canvas1.width, canvas1.height));
 
 
 
 
 
-
-
-/*---------SECOND DOT CANVAS------------------------*/
-            const canvas2 = document.getElementById("canvas2");
+   /*---------circle 2 canvas---------------------*/
+            const canvas2 = document.getElementById('canvas2');
 
             /* clear canvas button */
-            const clearEl2 = document.getElementById("clear2");
+            const clearEl2 = document.getElementById('clear2');
             
             const ctx2 = canvas2.getContext('2d');
             
             /* initialize global variables */
-            let radius = 3;           /* this is the circle radius and the line width */
-            let isPressed = true;
-            let color = '#0d2d53';
-            let currPosX, currPosY; /* position of the mouse; they start out undefined */
+            let radius2 = 3;           /* this is the circle radius and the line width */
+            let isPressed2 = true;
+            let color2 = '#0d2d53';
+            let currPosX2, currPosY2; /* position of the mouse; they start out undefined */
             
             /* set up the event listeners */
-            canvas2.addEventListener('mousedown', (e2) => {
-               isPressed = true;
+            canvas2.addEventListener('mousedown', (e) => {
+               isPressed2 = true;
             
-               currPosX = e2.offsetX;
-               currPosY = e2.offsetY;
+               currPosX2 = e.offsetX;
+               currPosY2 = e.offsetY;
             
                /* I think we want to draw a circle when the mouse is just clicked */
                /* not just when the mouse is moved */
                /* I changed this from the original program */
-               drawCircle(currPosX, currPosY);
+               drawCircle2(currPosX2, currPosY2);
             
                // console.log(isPressed, currPosX, currPosY);
             });
             
             canvas2.addEventListener('mouseup', (e) => {
-               isPressed = false;
+               isPressed2 = false;
             
-               currPosX = undefined;
-               currPosY = undefined;
+               currPosX2 = undefined;
+               currPosY2 = undefined;
             
                // console.log(isPressed, currPosX, currPosY);
             }); /* end mouseup eventListener */
             
-            canvas2.addEventListener('mousemove', (e2) => {
-               if( isPressed) {  /* we only want to do this when the mouse is held down */
-                  const x2 = e2.offsetX;
-                  const y2 = e2.offsetY;
+            canvas2.addEventListener('mousemove', (e) => {
+               if( isPressed2) {  /* we only want to do this when the mouse is held down */
+                  const x22 = e.offsetX;
+                  const y22 = e.offsetY;
             
-                  drawCircle(x2, y2); /* but this can't keep up with fast movement, so draw line also */
-                  drawLine(currPosX, currPosY, x2, y2);
+                  drawCircle2(x22, y22); /* but this can't keep up with fast movement, so draw line also */
+                  drawLine2(currPosX2, currPosY2, x22, y22);
             
                   /* we need to update the current position */
-                  currPosX = x2;
-                  currPosY = y2;
+                  currPosX2 = x22;
+                  currPosY2 = y22;
                   // console.log(offsetX, offsetY);
                } /* end isPressed */
             
@@ -145,26 +146,132 @@ clearEl.addEventListener('click', () =>
             /* radius is the radius */
             /* the next two are start angle and end angle in RADIANS */
             /* 2*PI is 360 degrees */
-            function drawCircle( x, y ) {
+            function drawCircle2(x2, y2) {
                ctx2.beginPath();
-               ctx2.arc(x, y, radius, 0, Math.PI * 2);
-               ctx2.fillStyle = color;
+               ctx2.arc(x2, y2, radius2, 0, Math.PI * 2);
+               ctx2.fillStyle = color2;
                ctx2.fill();
             } /* end drawCircle() */
             
             /* draw lines in between the circles to fill in the drawing */
             /* move to the first coordinate pair, draw to the second */
-            function drawLine(moveToX, moveToY, lineToX, lineToY) {
+            function drawLine2(moveToX2, moveToY2, lineToX2, lineToY2) {
                ctx2.beginPath();
-               ctx2.moveTo(moveToX, moveToY);
-               ctx2.lineTo(lineToX, lineToY);
-               ctx2.strokeStyle = color;
+               ctx2.moveTo(moveToX2, moveToY2);
+               ctx2.lineTo(lineToX2, lineToY2);
+               ctx2.strokeStyle = color2;
             
                /* we want diameter so line width matches circle */
-               ctx2.lineWidth = radius * 2;
+               ctx2.lineWidth = radius2 * 2;
                ctx2.stroke();                /* actually draw the line */
             }  /* end drawLine() */
             
             /* the clear button */
             clearEl2.addEventListener('click', () => 
                         ctx2.clearRect(0, 0, canvas2.width, canvas2.height));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*-----circle 2 canvas-------------------------------------*/
+// const canvas2 = document.getElementById('canvas2');
+
+// /* clear canvas button */
+// const clearEl2 = document.getElementById('clear2');
+
+// const ctx2 = canvas2.getContext('2d');
+
+// /* initialize global variables */
+// let radius2 = 3;           /* this is the circle radius and the line width */
+// let isPressed2 = true;
+// let color2 = '#0d2d53';
+// let currPosX2, currPosY2; /* position of the mouse; they start out undefined */
+
+// /* set up the event listeners */
+// canvas2.addEventListener('mousedown', (e) => {
+//    isPressed2 = true;
+
+//    currPosX2 = e.offsetX;
+//    currPosY2 = e.offsetY;
+
+//    /* I think we want to draw a circle when the mouse is just clicked */
+//    /* not just when the mouse is moved */
+//    /* I changed this from the original program */
+//    drawCircle(currPosX2, currPosY2);
+
+//    // console.log(isPressed, currPosX, currPosY);
+// });
+
+// canvas2.addEventListener('mouseup', (e) => {
+//    isPressed2 = false;
+
+//    currPosX2 = undefined;
+//    currPosY2 = undefined;
+
+//    // console.log(isPressed, currPosX, currPosY);
+// }); /* end mouseup eventListener */
+
+// canvas2.addEventListener('mousemove', (e) => {
+//    if( isPressed2) {  /* we only want to do this when the mouse is held down */
+//       const x22 = e.offsetX;
+//       const y22 = e.offsetY;
+
+//       drawCircle(x22, y22); /* but this can't keep up with fast movement, so draw line also */
+//       drawLine(currPosX2, currPosY2, x22, y22);
+
+//       /* we need to update the current position */
+//       currPosX2 = x22;
+//       currPosY2 = y22;
+//       // console.log(offsetX, offsetY);
+//    } /* end isPressed */
+
+//    // console.log(isPressed, currPosX, currPosY);
+// }); /* end mousemove eventListener */
+
+// /* x, y define center of circle */
+// /* radius is the radius */
+// /* the next two are start angle and end angle in RADIANS */
+// /* 2*PI is 360 degrees */
+// function drawCircle(x, y) {
+//    ctx2.beginPath();
+//    ctx2.arc(x, y, radius2, 0, Math.PI * 2);
+//    ctx2.fillStyle = color2;
+//    ctx2.fill();
+// } /* end drawCircle() */
+
+// /* draw lines in between the circles to fill in the drawing */
+// /* move to the first coordinate pair, draw to the second */
+// function drawLine(moveToX, moveToY, lineToX, lineToY) {
+//    ctx2.beginPath();
+//    ctx2.moveTo(moveToX, moveToY);
+//    ctx2.lineTo(lineToX, lineToY);
+//    ctx2.strokeStyle = color2;
+
+//    /* we want diameter so line width matches circle */
+//    ctx2.lineWidth = radius2 * 2;
+//    ctx2.stroke();                /* actually draw the line */
+// }  /* end drawLine() */
+
+// /* the clear button */
+// clearEl2.addEventListener('click', () => 
+//             ctx2.clearRect(0, 0, canvas2.width, canvas2.height));
+
